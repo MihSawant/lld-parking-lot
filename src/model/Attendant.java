0e544be081142c8d7b1fd97fd91fa4fc8485b0e8
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-11 11:33:49 +0530

    added Attendant model

---
+++ src/model/Attendant.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Attendant {
+
+    private String name;
+
+    public double collectFees(double amountPaid){
+        // TODO amount paid minus original amount
+        return 0.0;
+    }
+}
