8d466fa1d6202634d01915cfd5398a7629e9ab05
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-11 11:34:24 +0530

    added system package and parking lot

---
+++ src/system/ParkingLot.java
@@ -0,0 +1,14 @@
+package system;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ParkingLot {
+
+    // Using parking lot as a Map so Floor no and parking spots on that floor
+    /*
+        In this case i am assuming that Their are 3 Floors and Each floor has 6 spots
+        so in total 18 spots
+     */
+    private HashMap<Integer, List<Integer>> parkingLot;
+}
