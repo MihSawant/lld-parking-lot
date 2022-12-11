4e76d4517ef7b56fe73d21133b9d82c9cee4353b
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-11 22:07:09 +0530

    updated the parking lot to hold customer data

--- src/system/ParkingLot.java
+++ src/system/ParkingLot.java
@@ -1,7 +1,11 @@
 package system;
 
+import model.Customer;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParkingLot {
 
@@ -10,5 +14,16 @@ public class ParkingLot {
         In this case i am assuming that Their are 3 Floors and Each floor has 6 spots
         so in total 18 spots
      */
-    private HashMap<Integer, List<Integer>> parkingLot;
+    private Map<Integer, List<Customer>> parkingLot;
+    public ParkingLot(){
+        parkingLot = new HashMap<>();
+        parkingLot.put(1, new ArrayList<Customer>(8));
+        parkingLot.put(2, new ArrayList<Customer>(8));
+        parkingLot.put(3, new ArrayList<Customer>(8));
+    }
+
+    public int getSize(){
+        return parkingLot.size();
+    }
+
 }
