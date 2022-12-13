ae68a44bc9110b53b1057987fca933cc181bda0c
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-13 09:12:31 +0530

    added park method in parking lot

--- src/system/ParkingLot.java
+++ src/system/ParkingLot.java
@@ -2,10 +2,7 @@ package system;
 
 import model.Customer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ParkingLot {
 
@@ -14,7 +11,7 @@ public class ParkingLot {
         In this case i am assuming that Their are 3 Floors and Each floor has 6 spots
         so in total 18 spots
      */
-    private Map<Integer, List<Customer>> parkingLot;
+    private Map<java.lang.Integer, List<Customer>> parkingLot;
     public ParkingLot(){
         parkingLot = new HashMap<>();
         parkingLot.put(1, new ArrayList<Customer>(8));
@@ -22,8 +19,24 @@ public class ParkingLot {
         parkingLot.put(3, new ArrayList<Customer>(8));
     }
 
+
+    public void park(Customer car){
+        Collection<List<Customer>> spots = parkingLot.values();
+        long count = spots.stream()
+                .map(List::size)
+                .count();
+        if(count > 24) System.out.println("Sorry, All the parking spots are full");
+        else{
+            System.out.println("Place available");
+        }
+    }
+
     public int getSize(){
         return parkingLot.size();
+    }
+
+    public Map<java.lang.Integer, List<Customer>> getLot(){
+        return Map.copyOf(parkingLot);
     }
 
 }
