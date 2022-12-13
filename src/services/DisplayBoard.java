791cb3381e9837849b95625802c47be304a0e7aa
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-13 09:13:09 +0530

    added services

---
+++ src/services/DisplayBoard.java
@@ -0,0 +1,21 @@
+package services;
+
+import model.Customer;
+import system.ParkingLot;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class DisplayBoard {
+
+    Map<java.lang.Integer, List<Customer>> map;
+    public DisplayBoard(){
+        map = new ParkingLot().getLot();
+    }
+
+    public void printSpots(){
+        map.forEach((floor, spots) -> System.out.println(floor+" "+ spots));
+    }
+
+}
