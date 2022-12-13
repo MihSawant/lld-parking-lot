0c602cc2984638446986f1281d1656bb3a93e3b9
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-13 09:13:39 +0530

    updated code little bit

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,16 @@
 import model.Customer;
-import model.ParkingTicket;
+import services.DisplayBoard;
+import system.ParkingLot;
 
 public class Test {
     public static void main(String[] args) {
         // Adding new Customer
         Customer c = new Customer("Mihir", "MH-98-1121");
         c.printCustomerDetails();
+        ParkingLot lot = new ParkingLot();
+        lot.park(c);
+//        DisplayBoard board = new DisplayBoard();
+//        board.printSpots();
+
     }
 }
